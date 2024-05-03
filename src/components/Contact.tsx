df2b5d6bf4306801587948bef929c0b63b954f41
DevMarioValdez <marioaf.valdez@gmail.com>
2024-05-03 14:17:39 -0700

    Update UI

---
+++ src/components/Contact.tsx
@@ -0,0 +1,9 @@
+const Contact = () => {
+    return (
+        <section>
+            <div className="w-full max-w-6xl mx-auto px-2 md:px-2 mr-20 "></div>
+        </section>
+    );
+};
+
+export default Contact;
